1acfb49bc37096091bbf06d3dcf42f179fef8a25
mcelrea <mcelrea@damien-hs.edu>
2013-03-26 13:31:53 -0700

    new player image

---
+++ Charlesburg/src/com/damien/sprites/Enemy.java
@@ -0,0 +1,13 @@
+package com.damien.sprites;
+
+import org.newdawn.slick.Image;
+
+public class Enemy extends Sprite{
+
+	public Enemy(Image i) {
+		super(i);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
